1400e48ba9128a6c110c6446cb25c27d94fde893
tushar <tus123@gmail.com>
2025-07-06 16:51:47 +0530

    versio-1

---
+++ one.py
@@ -0,0 +1,3 @@
+word = "pop"
+is_palindrome = word == word[::1]
+print("is palindrome",is_palindrome)
---
+++ palindrome.py
@@ -0,0 +1,4 @@
+word = "tushar"
+is_palindrome = word == word [::-1]
+
+print("Is palindrome :", is_palindrome)
